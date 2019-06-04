91aadd431e6349d1882f7462c37f1180fc05d840
aaronmoran <aaron.moran@nbrown.co.uk>
2019-06-04 10:42:25 +0100

    added method to square that reduces number of options available and reports outcome

--- src/main/java/com/sudoku/boardObjects/Square.java
+++ src/main/java/com/sudoku/boardObjects/Square.java
@@ -1,15 +1,31 @@
 package com.sudoku.boardObjects;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Square {
-	int value;
-	ArrayList<Integer> options;
-	boolean isSet;
+	
+	public static String VALUE_SET = "value set";
+	
+	public static String NO_OPTIONS = "no options";
+	
+	public static String OPTIONS_REMOVED = "options removed";
+	
+	public static String NO_OPTIONS_REMOVED = "no options removed";
+	
+	public static String SQUARE_ALREADY_SET = "square already set";
+	
+	private int value;
+	
+	private Set<Integer> options;
+	
+	private boolean isSet;
 	
 	public Square() {
 		this.isSet = false;
-		this.options = new ArrayList<Integer>();
+		this.options = new HashSet<Integer>();
 		for (int i = 0; i < 10 ; i++) {
 			this.options.add(i);
 		}
@@ -20,20 +36,50 @@ public class Square {
 		this.isSet = true;
 		
 	}
+	
 	public int getValue() {
 		return value;
 	}
+	
 	public void setValue(int value) {
 		this.value = value;
 		this.isSet = true;
 	}
-	public ArrayList<Integer> getOptions() {
+	
+	public Set<Integer> getOptions() {
 		return options;
 	}
-	public void setOptions(ArrayList<Integer> options) {
+	
+	public void setOptions(Set<Integer> options) {
 		this.options = options;
 	}
+	
 	public boolean isSet() {
 		return isSet;
+	}
+	
+	public String removeOptionsReportOutcome(ArrayList<Integer> optionsToRemove) {
+		if (isSet) {
+			return SQUARE_ALREADY_SET;
+		}
+		boolean optionsRemoved = false;
+		for (int i = 0; i < optionsToRemove.size(); i++) {
+			boolean isRemoved = options.remove(optionsToRemove.get(i));
+			if (isRemoved) {
+				optionsRemoved = true;
+			}
+		}
+		if (options.isEmpty()) {
+			return NO_OPTIONS;
+		} else if (options.size() == 1) {
+			Iterator<Integer> iterator = options.iterator();
+			int value = iterator.next();
+			setValue(value);
+			return VALUE_SET;
+		} else if (optionsRemoved) {
+			return OPTIONS_REMOVED;
+		} else {
+			return NO_OPTIONS_REMOVED;
+		}
 	}
 }
