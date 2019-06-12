2911e6a7f867767bfe6c12ddf979351b12bb2014
aaronmoran <aaron.moran@nbrown.co.uk>
2019-06-12 15:56:44 +0100

    created service to clone boards

---
+++ src/main/java/com/sudoku/services/BoardCloneService.java
@@ -0,0 +1,20 @@
+package com.sudoku.services;
+
+import org.springframework.stereotype.Service;
+
+import com.sudoku.boardObjects.Board;
+
+@Service
+public class BoardCloneService {
+	
+	public static Board cloneBoard(Board board) {
+		if (board.isUnsolvable() || board.isSolved()) {
+			System.out.println("ERROR: trying to clone board that is solved or unsolveable");
+			return null;
+		}
+		Board cloneBoard = new Board();
+		cloneBoard.setBoardFromListOfStringValues(board.getBoardAsListOfStringValues());
+		return cloneBoard;
+	}
+
+}
